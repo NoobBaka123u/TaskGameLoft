7db3aedc04f92fb6bc0fcca86d05bf7a26f73f81
= <=>
2019-06-17 13:14:45 +0700

    +Add Constructor into Patient.cpp

--- PatientFightVirus/PatientFightVirus/Patient.cpp
+++ PatientFightVirus/PatientFightVirus/Patient.cpp
@@ -4,9 +4,16 @@
 
 Patient::Patient()
 {
+	this->m_resistance = 1;
+	this->m_state = 1;
+	this->DoStart();
 }
 
 
 Patient::~Patient()
+{
+}
+
+void Patient::SetResistancePatient(int m_resistance)
 {
 }
--- PatientFightVirus/PatientFightVirus/Patient.h
+++ PatientFightVirus/PatientFightVirus/Patient.h
@@ -1,8 +1,24 @@
 ﻿#pragma once
+#include "Flu.h"
+#include "Dengue.h"
+
 class Patient
 {
+private:
+	int m_resistance;
+	int m_state;
+protected:
+	list<Pathogen*>m_pathogenList;
+
 public:
 	Patient();
 	~Patient();
-};
 
+	void SetResistancePatient(int m_resistance);
+	int GetResistancePatient();
+	void InitResistance();
+	void DoStart();
+	void TakeMedicine(int medicineResitance);
+	void DoDie();
+	int GetState();
+};
