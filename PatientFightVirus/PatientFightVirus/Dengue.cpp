f71ec1a35fa89ccddf94f65be9e54fdc6bfd54c6
= <=>
2019-06-17 13:09:58 +0700

    +Add Constructor into Dengue.cpp

--- PatientFightVirus/PatientFightVirus/Dengue.cpp
+++ PatientFightVirus/PatientFightVirus/Dengue.cpp
@@ -4,9 +4,31 @@
 
 Dengue::Dengue()
 {
+	this->DoBorn();
+}
+
+Dengue::Dengue(const Dengue * pathogen)
+{
 }
 
 
 Dengue::~Dengue()
+{
+}
+
+void Dengue::DoBorn()
+{
+}
+
+list<Pathogen*> Dengue::DoClone()
+{
+	return list<Pathogen*>();
+}
+
+void Dengue::DoDie()
+{
+}
+
+void Dengue::InitResistance()
 {
 }
--- PatientFightVirus/PatientFightVirus/Dengue.h
+++ PatientFightVirus/PatientFightVirus/Dengue.h
@@ -1,8 +1,18 @@
 #pragma once
-class Dengue
+#include "Pathogen.h"
+#define BUFFER_SIZE 4
+
+class Dengue : public Pathogen
 {
+private:
+	char m_protein[BUFFER_SIZE];
+
 public:
 	Dengue();
+	Dengue(const Dengue* pathogen);
 	~Dengue();
+	void DoBorn();
+	list<Pathogen*> DoClone();
+	void DoDie();
+	void InitResistance();
 };
-
