e164466d3f40d196a20c485c2d81385f037844a8
= <=>
2019-06-17 13:13:07 +0700

    +Add InitResistance() into Dengue.cpp

--- PatientFightVirus/PatientFightVirus/Dengue.cpp
+++ PatientFightVirus/PatientFightVirus/Dengue.cpp
@@ -40,4 +40,27 @@ void Dengue::DoDie()
 
 void Dengue::InitResistance()
 {
+
+	int r = rand() % 3 + 1;
+	if (r == 1)
+	{
+		strncpy_s(this->m_protein, "NS3", BUFFER_SIZE - 1);
+		r = rand() % 10 + 1;
+		this->SetResistancePathogen(r);
+		cout << "NS3 Dengue" << " " << "Risistance:" << GetResistancePathogen() << endl;
+	}
+	else if (r == 2)
+	{
+		strncpy_s(this->m_protein, "NS5", BUFFER_SIZE - 1);
+		r = rand() % 20 + 11;
+		this->SetResistancePathogen(r);
+		cout << "NS5 Dengue" << " " << "Risistance:" << this->GetResistancePathogen() << endl;
+	}
+	else
+	{
+		strncpy_s(this->m_protein, "E", BUFFER_SIZE - 1);
+		r = rand() % 30 + 21;
+		this->SetResistancePathogen(r);
+		cout << "E Dengue" << " " << "Risistance:" << this->GetResistancePathogen() << endl;
+	}
 }
